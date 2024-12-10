255b5d8ec685e641c8d90b456148d6e3dcbee30b
Sherina Eria <hastalinas@gmail.com>
2024-12-10 21:11:42 +0700

    push baru

--- a.js
+++ a.js
@@ -1,5 +1,14 @@
 // ini isi dari a.js
+
+// PUSH
 // git add nama file
 // git commit -m "comment"
 // git branch -M main
 // git push -u origin main
+
+// git checkout -b branch sherin
+
+// REVERT (undo ketika ada yang salah) 
+// git log --oneline
+// git revert 2314
+// pencet i -> tulis komen -> :wq
